a89f1c82f34cf03b922591bf7fb87bb7ab79a07a
tstikvoort <tstikvoort@hotmail.com>
2015-08-28 22:49:13 +0200

    minor changes

--- client/templates/home.js
+++ client/templates/home.js
@@ -1,12 +1,3 @@
-letTheMagicBegin = function() {
-	magic = setInterval(function(){
-		$('.flexbox .tile:nth-child(2n+1)').toggleClass('magic');
-	}, 1000);
-}
-zoIsHetGenoeg = function() {
-	clearTimeout(magic);
-}
-
 Meteor.subscribe('projects');
 Template.home.helpers({
 	projects: function() {
